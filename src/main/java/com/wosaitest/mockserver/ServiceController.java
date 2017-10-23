6f2bbf935a951e8d73938557c819cf1a2ac44553
yangzhixiang <yangzhixiang@wosai-inc.com>
2017-10-23 14:48:12 +0800

    add user object

--- src/main/java/com/wosaitest/mockserver/ServiceController.java
+++ src/main/java/com/wosaitest/mockserver/ServiceController.java
@@ -1,56 +1,58 @@
 package com.wosaitest.mockserver;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.UriInfo;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
- * 
  * @author yangzhixiang
- *
  */
 @Path("/mockserver")
 public class ServiceController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
 
-	private AtomicInteger ai = new AtomicInteger(0);
+    private AtomicInteger ai = new AtomicInteger(0);
 
-	@GET
-	@Path("/status")
-	@Produces("application/json; charset=GBK")
-	public String getMNS(@Context UriInfo uriInfo, String body) {
-		MultivaluedMap<String, String> multivaluedMap = uriInfo.getQueryParameters();
-		logger.info("status get: " + multivaluedMap.toString() + "-----" + body);
-		return String.valueOf(this.ai.get());
-	}
+    @GET
+    @Path("/status")
+    @Produces("application/json; charset=GBK")
+    public String getMNS(@Context UriInfo uriInfo, String body) {
+        MultivaluedMap<String, String> multivaluedMap = uriInfo.getQueryParameters();
+        logger.info("status get: " + multivaluedMap.toString());
+        return String.valueOf(this.ai.get());
+    }
 
-	@GET
-	@Path("/reset")
-	@Produces("application/json; charset=GBK")
-	public String resetMNS(@Context UriInfo uriInfo, String body) {
-		this.ai.set(0);
-		logger.info("reset ok");
-		return "{\"msg\":\"reset ok\"}";
-	}
+    @GET
+    @Path("/reset")
+    @Produces("application/json; charset=GBK")
+    public String resetMNS(@Context UriInfo uriInfo, String body) {
+        this.ai.set(0);
+        logger.info("reset ok");
+        return "{\"msg\":\"reset ok\"}";
+    }
 
-	@POST
-	@Path("/createmsg")
-	@Produces("application/json")
-	@Consumes(value = { "text/xml", "application/json" })
-	public String createGetuiMsg(@Context String body) {
-		logger.info("get create msg " + body);
-		this.ai.incrementAndGet();
-		return "{\"result\":\"ok\"}";
-	}
+    @POST
+    @Path("/createmsg")
+    @Produces("application/json; charset=utf-8")
+    @Consumes(value = {"application/json"})
+    public User createGetuiMsg(@Context UriInfo uriInfo, String body) {
+        User user = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            user = mapper.readValue(body, User.class);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+        this.ai.incrementAndGet();
+        return new User(user.getName(), user.getAge());
+    }
 }
---
+++ src/main/java/com/wosaitest/mockserver/User.java
@@ -0,0 +1,35 @@
+package com.wosaitest.mockserver;
+
+/**
+ * Created by yangzhixiang on 2017/9/26.
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
